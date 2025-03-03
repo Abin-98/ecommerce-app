833f46f301a73b68cd0c9e1dba5212bfa0d44197
Abin-98 <abinvarkey01998@gmail.com>
2025-03-03 23:09:17 +0530

    admin wip

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
-import Sidebar from './components/Sidebar'
-import Navbar from './components/Navbar'
+import React, { useState } from "react";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Orders from "./pages/Orders";
+import Login from "./components/Login";
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL
 
 const App = () => {
+  const [token, setToken] = useState("");
   return (
-    <div className='bg-gray=50 min-h-screen'>
-      <>
-    <Navbar/>
-    <hr/>
-    <div className='flex w-full'>
-      <Sidebar/>
+    <div className="bg-gray=50 min-h-screen">
+      {token === "" ? (
+        <Login />
+      ) : (
+        <>
+          <Navbar />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<Add />} />
+                <Route path="/add" element={<List />} />
+                <Route path="/add" element={<Orders />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
     </div>
-    </>
-      
-    </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
