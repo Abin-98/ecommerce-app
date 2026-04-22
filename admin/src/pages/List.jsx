5200049d4f6e95c01ace7d12c4a1bbb7354c73c2
Abin Varkey <abinvarkey01998@gmail.com>
2026-04-22 17:17:00 +0530

    admin add page done, jwt authentication integrated

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,10 +1,36 @@
+import axios from 'axios'
 import React from 'react'
+import { useEffect } from 'react'
+import { backendUrl } from '../App'
+import { toast } from 'react-toastify'
+import { useState } from 'react'
 
 const List = () => {
+  const [list, setList] = useState([])
+
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(backendUrl + '/api/product/list')
+      if (response.data.success) {
+        setList(response.data.products)
+      } else {
+        toast.error(response.data.message)
+      }
+      
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message)
+    }
+  }
+
+  useEffect(()=>{
+    fetchList()
+  }, [])
+
   return (
-    <div>
-        
-    </div>
+    <>
+        <p className=''>All Products List</p>
+    </>
   )
 }
 
